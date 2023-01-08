7dd5d70b1c34d979c64ef9baf54e92d0b53ef16a
Marques Ivory <Maivory12@icloud.com>
2023-01-08 17:52:29 -0500

    Added array of questions for user input about project

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the name of your project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description and functionality of this project.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select license that applies to this project.",
+        chioces: ["None","MIT","APACHE 2.0","BSD","GPLV2","GPLV3","ISC"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "List the steps needed to install your project.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter the languages of technologies associated with your project.",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please list any contributers to your project.",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "If this applies, please provide a walkthrough of required test.",
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "Enter email address to provied a way to be reached for questions.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
